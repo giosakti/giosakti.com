5f659049266b20c78cca380288bda405ee17e973
Giovanni Sakti <giosakti@gmail.com>
2024-06-16 10:48:12 +0700

    feat: update layout of the homepage

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 import type { Metadata, Site, Socials } from "@types";
 
 export const SITE: Site = {
-  TITLE: "giosakti.com",
+  TITLE: "giosakti",
   DESCRIPTION: "Giovanni Sakti, Software Engineer and Writer.",
   EMAIL: "",
   NUM_POSTS_ON_HOMEPAGE: 5,
